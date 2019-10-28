20ff8e8d15cc7c3fc6a935f8191bd09cd1a3d2c0
Ritvars Jakucevičs <ritvarsjakucevics@gmail.com>
2019-10-28 18:45:27 +0200

    First part of UI

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
 
 }
