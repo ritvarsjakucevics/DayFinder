90d7afbd2a28d7d77e65d905445b76b9a6c96430
Ritvars Jakucevičs <ritvarsjakucevics@gmail.com>
2019-10-28 18:14:25 +0200

    Testing first commit.

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dayF = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
