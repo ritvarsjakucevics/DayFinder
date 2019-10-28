db2169c08c8fdaf505d33e02f3a18c86e1a21524
Ritvars Jakucevičs <ritvarsjakucevics@gmail.com>
2019-10-28 18:09:45 +0200

    Initial Commit

---
+++ DayFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DayFinder
+//
+//  Created by Ritvars Jakucevičs on 28/10/2019.
+//  Copyright © 2019 Ritvars Jakucevičs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
